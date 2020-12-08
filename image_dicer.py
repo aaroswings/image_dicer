e4ec610acb16b4fbc9e6be83ef7aaae657be3ccc
aaroswings <35984064+aaroswings@users.noreply.github.com>
2020-12-07 17:44:17 -0800

    Update image_dicer.py

--- image_dicer.py
+++ image_dicer.py
@@ -6,17 +6,16 @@ from PIL import Image
 Image.MAX_IMAGE_PIXELS = None
 
 parser = argparse.ArgumentParser(description='IMAGE CHIPPING FUNCTIONS')
-parser.add_argument('-i', '--input_dir', type=str, metavar='str', help='path to input files to be processed')
-parser.add_argument('-o', '--output_dir', type=str, metavar='str', help='path to output directory')
+parser.add_argument('-i', '--source_dir', type=str, metavar='str', help='path to input files to be processed')
+parser.add_argument('-o', '--dest_dir', type=str, metavar='str', help='path to output directory')
 parser.add_argument('--max_size', type=int, default=2048, metavar='str',
                     help='maximum size of long edge')
 parser.add_argument('--max_edge_ratio', type=int, default=1.0, metavar='str', help='maximum image edge ratio')
 parser.add_argument('--fileglob', nargs='+', help='File extensions of input in form *.png, *.jpg etc')
 
 opt = parser.parse_args()
-print(opt)
 
-def resize_by_long_edge(img, max_size=512):
+def resize_by_long_edge(img, max_size=2048):
     original_size = max(img.size[0], img.size[1])
 
     if original_size >= max_size:
@@ -27,7 +26,7 @@ def resize_by_long_edge(img, max_size=512):
             resized_height = max_size
             resized_width = int(round((max_size/float(img.size[1]))*img.size[0]))
 
-        img = img.resize((resized_width, resized_height), img.ANTIALIAS)
+        img = img.resize((resized_width, resized_height), Image.ANTIALIAS)
     return img
 
 def crop_by_ratio(img, max_aspect_ratio=1.25):
@@ -72,18 +71,18 @@ def crop(img, height, width):
             box = (j*width, i*height, (j+1)*width, (i+1)*height)
             yield img.crop(box)
 
-if opt.input_dir is not None:
-    output_dir = opt.output_dir
-    if output_dir is None:
-        output_dir = opt.input_dir
+if opt.source_dir is not None:
+    dest_dir = opt.dest_dir
+    if dest_dir is None:
+        dest_dir = opt.source_dir
 
-    in_directory = Path(opt.input_dir)
+    in_directory = Path(opt.source_dir)
     for ext in opt.fileglob:
         for filename in in_directory.glob(ext):
             img = Image.open(filename)
             for crop_i, cropped in enumerate(crop_by_ratio(img, opt.max_edge_ratio)):
                 resized = resize_by_long_edge(cropped, opt.max_size)
-                with open(opt.output_dir / f'{filename.name}_slice_{crop_i}.jpg', 'wb+') as f:
+                with open(f'{opt.dest_dir}/{filename.name}_slice_{crop_i}.jpg', 'wb+') as f:
                     resized.save(f)
 
 
